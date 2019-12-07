123177c55c6ecf5543c682e21eb9d92e4c73c088
= <=>
2019-12-07 18:17:36 +0600

    move to fragments

---
+++ app/src/main/java/kz/kbtu/android/MainFragment.java
@@ -0,0 +1,28 @@
+package kz.kbtu.android;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainFragment extends Fragment {
+
+    RecyclerView movieListRC;
+    MovieAdapter adapter;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
